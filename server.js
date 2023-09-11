1baf1c983a2a4b14ea66c77e6730d436ef3ee5c6
raouf-allala <raouf.allala20@gmail.com>
2023-09-11 15:05:59 +0100

    fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://mystorify.netlify.app/',
     credentials: true,
   })
 );
