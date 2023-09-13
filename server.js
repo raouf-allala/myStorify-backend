6290ef2dbf5b1be1495ca11f5134f2f79e96867b
raouf-allala <raouf.allala20@gmail.com>
2023-09-13 16:06:20 +0100

    auth

--- server.js
+++ server.js
@@ -15,22 +15,23 @@ const cookieParser = require('cookie-parser');
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // cors whitelist
-var whitelist = [
-  'http://localhost:5173',
-  'https://mystorify.netlify.app',
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  })
-);
+// var whitelist = [
+//   'http://localhost:5173',
+//   'https://mystorify.netlify.app',
+// ];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(fileUpload({ useTempFiles: true }));
 app.use(passport.initialize());
 app.use(cookieParser());
