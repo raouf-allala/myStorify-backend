d96f31bf683f7460fcba7596d6a25d2387e8cdd5
raouf-allala <raouf.allala20@gmail.com>
2023-09-13 16:21:25 +0100

    cors fix

--- server.js
+++ server.js
@@ -15,23 +15,12 @@ const cookieParser = require('cookie-parser');
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // cors whitelist
-// var whitelist = [
-//   'http://localhost:5173',
-//   'https://mystorify.netlify.app',
-// ];
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (whitelist.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ALLOW,
+    credentials: true,
+  })
+);
 app.use(fileUpload({ useTempFiles: true }));
 app.use(passport.initialize());
 app.use(cookieParser());
